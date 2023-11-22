844f2543667026da793118963bbde06025e80d4c
Pravin Poudel <pravin.2047@gmail.com>
2023-11-23 00:48:26 +0545

    categories-update

--- modules/Configuration/Controllers/CategoryController.php
+++ modules/Configuration/Controllers/CategoryController.php
@@ -48,13 +48,7 @@ class CategoryController extends Controller
             ->withCategoryListData($categoryListData);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('Configuration::Category.create');
-    }
+ 
 
     /**
      * Store a newly created resource in storage.
@@ -92,118 +86,7 @@ class CategoryController extends Controller
         );
     }
 
-    /**
-     * Display the specified resource.
-     */
-
-    /**
-     * @OA\Get(
-     *     path="/api/fiscalyears/{fiscalyear}",
-     *     tags={"Fiscal Year"},
-     *     summary="Get a fiscal year by id",
-     *     description="This can only be done by the logged in user.",
-     *     operationId="showFiscalYear",
-     *     @OA\Parameter(
-     *         name="fiscalyear",
-     *         in="path",
-     *         description="Fiscal year id to view",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="integer",
-     *             format="int64"
-     *         ),
-     *     ),
-     *     @OA\Response(response=400,description="Invalid ID supplied"),
-     *     @OA\Response(response=404,description="Fiscal year not found"),
-     *     @OA\Response(response=200,description="Fiscal year found"),
-     *     security={{"bearer_token":{}}}
-     * )
-     */
-    public function show($id)
-    {
-        $data = $this->fiscalYears->find($id);
-        if ($data) {
-            return Response(['status' => 200, 'data' => $data, 'message' => 'Fiscal year found successfully.'], 200);
-        } else {
-            return Response(['status' => 404, 'message' => 'Fiscal year not found.'], 200);
-        }
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-
-    /**
-     * @OA\Put(
-     *     path="/api/fiscalyears/{fiscalyear}",
-     *     tags={"Fiscal Year"},
-     *     summary="Update fiscal year",
-     *     description="This can only be done by the logged in user.",
-     *     operationId="updateFiscalYear",
-     *     @OA\Parameter(
-     *         name="fiscalyear",
-     *         in="path",
-     *         description="Fiscal year id to update",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="integer",
-     *             format="int64"
-     *         ),
-     *     ),
-     *     @OA\Response(response=200,description="Fiscal year updated"),
-     *     @OA\RequestBody(
-     *         description="Create fiscal year object",
-     *         required=true,
-     *         @OA\JsonContent(ref="#/components/schemas/FiscalYear")
-     *     ),
-     *      security={{"bearer_token":{}}}
-     * )
-     */
-    public function update(FiscalYearRequest $request, $id)
-    {
-        $fiscalyear = $this->fiscalYears->update($id, $request->all());
-        if ($fiscalyear) {
-            return Response(['status' => 200, 'message' => 'Fiscal year updated successfully.'], 200);
-        } else {
-            return Response(['status' => 500, 'message' => 'Failed to update fiscal year.'], 200);
-        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-
-    /**
-     * @OA\Delete(
-     *     path="/api/fiscalyears/{fiscalyear}",
-     *     tags={"Fiscal Year"},
-     *     summary="Deletes a fiscal year",
-     *     description="This can only be done by the logged in user.",
-     *     operationId="deleteFiscalYear",
-     *     @OA\Parameter(
-     *         name="fiscalyear",
-     *         in="path",
-     *         description="Fiscal year id to delete",
-     *         required=true,
-     *         @OA\Schema(
-     *             type="integer",
-     *             format="int64"
-     *         ),
-     *     ),
-     *     @OA\Response(response=400,description="Invalid ID supplied"),
-     *     @OA\Response(response=404,description="Fiscal year not found"),
-     *     @OA\Response(response=200,description="Fiscal year successfully deleted"),
-     *     security={{"bearer_token":{}}}
-     * )
-     */
+    
     public function destroy($id)
     {
 
